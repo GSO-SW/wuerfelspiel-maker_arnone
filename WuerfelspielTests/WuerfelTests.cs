01f16df3765931846e1edc2a016c58ff9b42d8f8
Vansh Maker <vansh.m@SCHULE.KOELN>
2022-04-04 14:20:38 +0200

    Testfall für die Anzahl der Seiten

--- WuerfelspielTests/WuerfelTests.cs
+++ WuerfelspielTests/WuerfelTests.cs
@@ -8,20 +8,35 @@ namespace WuerfelspielTests
     public class WuerfelTests
     {
         [TestMethod]
-        public void DummyTest()
+        public void Wuerfel_DummyTest()
 
         {
             //Arange
-            WuerfelTests wuefel = new WuerfelTests()
-            Wuefel wuefel = new Wuefel= (6);
+            WuerfelTests wuefel = new WuerfelTests();
             int anzahlSeitenErgebnis = 6;
 
 
             //Act
+            Wuefel wuefel = new Wuefel(6);
 
             //Assert
             Assert.AreEqual(wuerfel.AnzahlSeiten, anzahlSeitenErgebnis);
        
+        }
+
+        [TestMethod]
+        public void Wuerfel_AnzahlSeiten()
+
+        {
+            //Arange
+            int anzahlseiten = 6;
+
+            //Act
+            Wuerfel wuerfel = new Wuerfel(anzahlseiten);
+
+            //Assert
+            Assert.AreEqual(wuerfel.AnzahlSeiten, anzahlseiten);
+
         }
     }
 }
